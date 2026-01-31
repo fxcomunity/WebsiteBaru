5c0a281f255f59a2c2c4abdc0b3361bdd4730439
fxcomunity <aqsholhalqi4@gmail.com>
2026-02-01 05:30:01 +0700

    Update music-data.js

--- music-data.js
+++ music-data.js
@@ -1,10 +1,10 @@
-// Music data from Sporty folder
+// Music data from GitHub Release v1
 const musicData = [
     {
         id: 1,
         title: "DJ EEEE AAAA ENGKOL",
         artist: "SENCY X SO ASU SLOW VIRAL TIKTOK",
-        file: "Sporty/DJ EEEE AAAA ENGKOL X SENCY X SO ASU SLOW VIRAL TIKTOK FULL SONG MAMAN FVNDY 2026_1080p.mp4",
+        file: "https://github.com/fxcomunity/WebsiteBaru/releases/download/v1/DJ.EAAA.mp4",
         duration: 0,
         favorite: false
     },
@@ -12,7 +12,7 @@ const musicData = [
         id: 2,
         title: "DJ KASIH ABA ABA",
         artist: "ELART REMIX - SHES TEN OUT OF TEN",
-        file: "Sporty/DJ KASIH ABA ABA ELART REMIX INI BUKAN CERPEN SHES TEN OUT OF TEN FULL SONG VIRAL TIKTOK 2025_1080p.mp4",
+        file: "https://github.com/fxcomunity/WebsiteBaru/releases/download/v1/DJ.KASIH.ABA.ABA.mp4",
         duration: 0,
         favorite: false
     },
@@ -20,7 +20,7 @@ const musicData = [
         id: 3,
         title: "DJ NGGAK DULU",
         artist: "Slowed & Reverb",
-        file: "Sporty/DJ NGGAK DULU (Slowed & Reverb)_720p.mp4",
+        file: "https://github.com/fxcomunity/WebsiteBaru/releases/download/v1/DJ.NGGAK.DULU.Slowed.Reverb._720p.mp4",
         duration: 0,
         favorite: false
     }
